f9a3c2ef331976155d908ffb57d4d93284fb4548
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-27 13:56:21 -0300

    a absic controller with simple endpoint: retrive all articles

---
+++ src/main/java/br/net/ti2/newsinlevelcontrolapi/controllers/ArticleController.java
@@ -0,0 +1,26 @@
+package br.net.ti2.newsinlevelcontrolapi.controllers;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.net.ti2.newsinlevelcontrolapi.dto.ArticleSimpleDto;
+import br.net.ti2.newsinlevelcontrolapi.repositories.ArticleRepository;
+
+@RestController
+@RequestMapping("/articles")
+public class ArticleController {
+	@Autowired
+	ArticleRepository articleRepository;
+	@GetMapping
+	public ResponseEntity<List<ArticleSimpleDto>> getAllArticles(){
+		List<ArticleSimpleDto> dtoList = articleRepository.findAll().stream().map(ArticleSimpleDto::new).collect(Collectors.toList());
+		return ResponseEntity.ok().body(dtoList);
+	}
+}
