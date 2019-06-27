58dc20cf5867519c022eae6772f861a59891c754
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-27 16:05:31 -0300

    add filter to query like comment sample

--- src/main/java/br/net/ti2/newsinlevelcontrolapi/repositories/ArticleRepository.java
+++ src/main/java/br/net/ti2/newsinlevelcontrolapi/repositories/ArticleRepository.java
@@ -1,5 +1,10 @@
 package br.net.ti2.newsinlevelcontrolapi.repositories;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import br.net.ti2.newsinlevelcontrolapi.models.Article;;
 
-public interface ArticleRepository extends JpaRepository<Article, Long>{}
+public interface ArticleRepository extends JpaRepository<Article, Long>{
+	//select order_number, title, done, id from articles where done < 5 and done > 0 order by updated_at ;
+	List<Article> findArticleByDoneLessThanAndDoneGreaterThanOrderByUpdatedAt(Long max,Long min);
+}
