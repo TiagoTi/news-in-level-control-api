d515f4bc3fd56743a20ffbd2198de9c58642dafe
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-27 16:05:48 -0300

    Add cors

--- src/main/java/br/net/ti2/newsinlevelcontrolapi/controllers/ArticleController.java
+++ src/main/java/br/net/ti2/newsinlevelcontrolapi/controllers/ArticleController.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,12 +16,14 @@ import br.net.ti2.newsinlevelcontrolapi.repositories.ArticleRepository;
 
 @RestController
 @RequestMapping("/articles")
+@CrossOrigin
 public class ArticleController {
 	@Autowired
 	ArticleRepository articleRepository;
 	@GetMapping
 	public ResponseEntity<List<ArticleSimpleDto>> getAllArticles(){
-		List<ArticleSimpleDto> dtoList = articleRepository.findAll().stream().map(ArticleSimpleDto::new).collect(Collectors.toList());
+		
+		List<ArticleSimpleDto> dtoList = articleRepository.findArticleByDoneLessThanAndDoneGreaterThanOrderByUpdatedAt(5L,0L).stream().map(ArticleSimpleDto::new).collect(Collectors.toList());
 		return ResponseEntity.ok().body(dtoList);
 	}
 }
