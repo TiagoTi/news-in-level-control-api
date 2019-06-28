c22e4b4933da6ad910a6087761962a5bde275a50
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-28 15:05:26 -0300

    update to more one done field

--- src/main/java/br/net/ti2/newsinlevelcontrolapi/controllers/ArticleController.java
+++ src/main/java/br/net/ti2/newsinlevelcontrolapi/controllers/ArticleController.java
@@ -3,15 +3,19 @@ package br.net.ti2.newsinlevelcontrolapi.controllers;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.net.ti2.newsinlevelcontrolapi.dto.ArticleSimpleDto;
+import br.net.ti2.newsinlevelcontrolapi.models.Article;
 import br.net.ti2.newsinlevelcontrolapi.repositories.ArticleRepository;
 
 @RestController
@@ -25,5 +29,14 @@ public class ArticleController {
 		
 		List<ArticleSimpleDto> dtoList = articleRepository.findArticleByDoneLessThanAndDoneGreaterThanOrderByUpdatedAt(5L,0L).stream().map(ArticleSimpleDto::new).collect(Collectors.toList());
 		return ResponseEntity.ok().body(dtoList);
+	}
+	
+	@PutMapping("{id}")
+	@Transactional
+	public ResponseEntity<ArticleSimpleDto> makeDone(@PathVariable Long id){
+		Article article = articleRepository.getOne(id);
+		article.setDone(article.getDone()+1);
+
+		return ResponseEntity.ok().body(new ArticleSimpleDto(article));
 	}
 }
