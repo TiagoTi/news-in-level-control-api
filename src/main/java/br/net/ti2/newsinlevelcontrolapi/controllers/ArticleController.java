a2eeea2875a2142e1a3223c1b041aa42c61a899c
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-07-03 21:26:05 -0300

    validate create

--- src/main/java/br/net/ti2/newsinlevelcontrolapi/controllers/ArticleController.java
+++ src/main/java/br/net/ti2/newsinlevelcontrolapi/controllers/ArticleController.java
@@ -1,19 +1,26 @@
 package br.net.ti2.newsinlevelcontrolapi.controllers;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import javax.transaction.Transactional;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import br.net.ti2.newsinlevelcontrolapi.dto.ArticleCreateDto;
 import br.net.ti2.newsinlevelcontrolapi.dto.ArticleSimpleDto;
 import br.net.ti2.newsinlevelcontrolapi.models.Article;
 import br.net.ti2.newsinlevelcontrolapi.repositories.ArticleRepository;
@@ -38,5 +45,12 @@ public class ArticleController {
 		article.setDone(article.getDone()+1);
 
 		return ResponseEntity.ok().body(new ArticleSimpleDto(article));
+	}
+	
+	@PostMapping
+	@Transactional
+	public ResponseEntity<ArticleSimpleDto> createArticle(@RequestBody @Valid ArticleCreateDto articleDto, UriComponentsBuilder uriBuilder){
+		URI location = uriBuilder.path("/articles").build().toUri();
+		return ResponseEntity.created(location).body(new ArticleSimpleDto());
 	}
 }
